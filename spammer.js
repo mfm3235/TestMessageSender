90c96df0298124d98d419292f92fcc33ff871410
mfm3235 <arasi.ctkp.531@gmail.com>
2025-03-17 15:36:31 +0900

    Update spammer.js

--- spammer.js
+++ spammer.js
@@ -1,63 +1,39 @@
-function S(e){window.enmity.plugins.registerPlugin(e)}
+function S(e) { window.enmity.plugins.registerPlugin(e); }
 
-const { React } = window.enmity.modules.common;
-const Messages = window.enmity.modules.common.Messages;
-const Channels = window.enmity.modules.common.Navigation;
+const { Messages, Commands, Navigation } = window.enmity.modules.common;
 
-
-const CustomButton = ({ channelId }) => {
-    const sendSpam = () => {
-        if (!channelId) return;
-        for (let i = 0; i < 10; i++) {
-            setTimeout(() => {
-                Messages.sendMessage(channelId, { content: "test" });
-            }, i * 300);
-        }
-    };
-
-    return React.createElement(
-        "button",
-        {
-            onClick: sendSpam,
-            style: {
-                backgroundColor: "#7289DA",
-                borderRadius: "5px",
-                padding: "8px",
-                marginLeft: "5px",
-                color: "white",
-                border: "none",
-                cursor: "pointer"
-            }
-        },
-        "連投"
-    );
+const sendSpam = (channelId) => {
+    if (!channelId) return;
+    for (let i = 0; i < 10; i++) { 
+        setTimeout(() => {
+            Messages.sendMessage(channelId, { content: "あ" });
+        }, i * 300); 
+    }
 };
 
 
-const patcher = window.enmity.patcher.create("spam-button-plugin");
-
 const plugin = {
-    name: "SpamButton",
+    name: "SpamCommand",
     version: "1.0.0",
-    description: "メッセージ送信ボタンの横に「連投」ボタンを追加し、「あ」を10回送信",
+    description: "「/start」と打つと「あ」を10回連投する",
     authors: [{ name: "YourName", id: "123456789012345678" }],
 
     onStart() {
-        patcher.after("MessageInput", "type", (args, res) => {
-            const channelId = Channels.getLastSelectedChannelId();
-            if (!channelId) return res;
-
-            const children = res?.props?.children;
-            if (children && Array.isArray(children)) {
-                children.push(React.createElement(CustomButton, { channelId }));
+        Commands.registerCommand({
+            name: "start",
+            displayName: "start",
+            description: "「あ」を10回連投する",
+            options: [],
+            execute: () => {
+                const channelId = Navigation.getLastSelectedChannelId();
+                sendSpam(channelId);
+                return { result: "スパム開始！" };
             }
-            
-            return res;
         });
     },
 
     onStop() {
-        patcher.unpatchAll();
+        Commands.unregisterCommand("start");
     }
 };
 
